58da12bba5c163bff54d98c40fe372475397fe00
Joshua Herring <jdherring2222@gmail.com>
2022-09-21 16:55:37 -0600

    inserted the code to worl properly

---
+++ solo_prep4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+            List<int> numbers = new List<int>();
+
+            Console.Write("Enter numbers to add to a list. Enter 0 to Finish.");
+
+            int number = -1;
+
+            while (number != 0)
+            {
+                string entry = Console.ReadLine();
+                    number = int.Parse(entry);
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+
+
+            int sum = numbers.Sum();
+            int length = numbers.Count;
+            Console.WriteLine($"The sum is {sum}");
+
+            Console.WriteLine($"The average is {sum/length}");
+
+            Console.WriteLine($"The largest number is {numbers.Max()}");
+
+
+
+
+
+
+
+        }
+    }
+}
