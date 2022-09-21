517a5a3e0134e7ad1d7e7c15176b37778aff1d21
Joshua Herring <jdherring2222@gmail.com>
2022-09-21 16:31:09 -0600

    inserted code from other file

---
+++ solo_prep3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Test
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            
+
+                        Random randomGenerator = new Random();
+            int goldenNumber = randomGenerator.Next(1, 9999);
+            int guess = 0;
+
+            while (guess != goldenNumber)
+
+            {
+                Console.Write("Guess a number any number?");
+                string stringNUmber = Console.ReadLine();
+                guess = int.Parse(stringNUmber);
+
+                if (guess < goldenNumber)
+                {
+                    Console.WriteLine("Too low try again.\n");
+                }
+
+                else if (guess > goldenNumber)
+                {
+                    Console.WriteLine("Too higher better luck next time.\n");
+                }
+
+                else
+                {
+                    Console.WriteLine("Bingo you got it champ!");
+                }
+
+            }
+
+
+
+        }
+    }
+}
