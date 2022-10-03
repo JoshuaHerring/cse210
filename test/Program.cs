309cffc9f4307641b5dccd3f6ca8aafe9c30d92e
Joshua Herring <jdherring2222@gmail.com>
2022-10-03 10:54:05 -0600

    test worked (omni sharp restart)

---
+++ test/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Director director = new Director();
+
+            director.type();
+
+        }
+    }
+}
---
+++ test/other/trying.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Test
+{
+    public class Director
+    {
+
+        public void type()
+        {
+            Console.WriteLine("Print to the console.");
+        }
+
+    }
+}
