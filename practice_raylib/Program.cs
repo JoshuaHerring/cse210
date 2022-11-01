d24f460658f4726a189ee305186f867dd8b8d4db
Joshua Herring <jdherring2222@gmail.com>
2022-11-01 15:52:33 -0600

    practiced with raylib

---
+++ practice_raylib/Program.cs
@@ -0,0 +1,31 @@
+﻿using Raylib_cs;
+using System;
+
+namespace Greed_Game
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int screenWidth = (Raylib.GetScreenWidth());
+            int screenHeight = (Raylib.GetScreenHeight());
+            Raylib.InitWindow(2200, 1600, "Creations");
+            Raylib.SetTargetFPS(100);
+            int x = 100;
+            int y = 100;
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.RAYWHITE);
+                Raylib.DrawCircle(x, y, 30, Color.SKYBLUE);
+                Raylib.DrawRectangle((x+200), (y+10), 200, 200, Color.DARKPURPLE);
+                Raylib.DrawFPS(0, 0);
+                x++;
+                y++;
+
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
